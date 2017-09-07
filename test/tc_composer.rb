88f738eb1669bc2c87b2d443a5ae85d4992741da
Chris Hudson <chudson@worldremit.com>
2017-09-07 09:40:58 +0100

    Fixed tests, added gemfile and a local setup shell script

--- test/tc_composer.rb
+++ test/tc_composer.rb
@@ -6,17 +6,17 @@ require 'minitest/reporters'
 require 'minitest/autorun'
 MiniTest::Reporters.use! [MiniTest::Reporters::DefaultReporter.new,
                           MiniTest::Reporters::JUnitReporter.new]
-require_relative '../lib/GitHubDownloader'
+require_relative '../lib/Composer'
 
 class TestComposer < MiniTest::Test
 
   def setup()
-    @compose = Composer.new('testdata/')
+    @compose = Composer.new("#{File.dirname(__FILE__)}/testdata/")
   end
 
   def test_initialize()
     assert_kind_of(Composer, @compose)
     assert_kind_of(Hash, @compose.config)
-    assert_equal('jwilder/nginx-proxy', compose.config['services']['nginx-proxy']['image'])
+    assert_equal('jwilder/nginx-proxy', @compose.config['services']['nginx-proxy']['image'])
   end
 end
